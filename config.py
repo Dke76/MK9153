5c5b7ce1390a81d74c09e3763174fed5d3756960
Dke76 <156434263+Dke76@users.noreply.github.com>
2024-01-21 13:54:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "6548002778:AAHjrz6DmmOqftU1cUEX3fikmnSW
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 5665231556)
+OWNER = int(os.environ.get("OWNER", 6381511858)
 
-LOG = -1002003120313
+LOG = -1002128466713
 
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "6573766001").split()):
+    for x in (os.environ.get("ADMINS", "5665231556").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
