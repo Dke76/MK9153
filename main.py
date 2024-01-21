0fae160503a5174a98df59fc51ded157e27c167b
Dke76 <156434263+Dke76@users.noreply.github.com>
2024-01-21 13:41:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ async def account_login(bot: Client, m: Message):
                         time.sleep(e.x)
                         continue
                 else:
-                    prog = await m.reply_text(f"**Downloading 📥:-\n\nVideo Name🚦:- {name}\nQuality 📹 - {raw_text2}\nLink 🔗 :{url}\n\n **bot made by all classes patna❤️**")
+                    prog = await m.reply_text(f"**Downloading 📥:-\n\nVideo Name🚦:- {name}\nQuality 📹 - {raw_text2}\nLink 🔗 :{url}\n\n **bot made by DEEPAK❤️**")
                     
                     
                     res_file = await helper.download_video(url, cmd, name)
